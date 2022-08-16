4afc0605185236ce11dc75f90d62ccebce3a7148
TAHAWY <70866079+tahawy111@users.noreply.github.com>
2022-08-16 14:37:12 +0200

    add discount

--- src.js
+++ src.js
@@ -1,4 +1,4 @@
-function repeatStr(
+export function repeatStr(
   str /* hear add Sting */,
   num /* hear add the number that you want to repeat the string */
 ) {
@@ -8,3 +8,13 @@ function repeatStr(
   }
   return final;
 }
+
+export function discount(regulerPrice, discount) {
+  const discountP = 100 - discount;
+
+  const sale = +`0.${discountP}` * regulerPrice;
+
+  return sale;
+}
+
+console.log(discount(1500, 15));
